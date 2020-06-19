b36ea505dc75d196853462394f247bab60cefd7e
chris <christoph.wagner@edu.campus02.at>
2020-06-19 13:27:38 +0200

    profiling disabled

--- v1.0/lasertools.py
+++ v1.0/lasertools.py
@@ -56,7 +56,7 @@ if "errormsg" not in dir(inkex):
 # Styles and additional parameters
 ################################################################################
 
-PROFILING = True   # Disable if not debugging
+PROFILING = False   # Disable if not debugging
 DEBUG = False      # Disable if not debugging
 TINY_INFILL_FACTOR = 2  # x times the laser beam width will be removed
 ENGRAVING_TOLERANCE = 0.000002
